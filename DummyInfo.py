608b5ee5fe8fd2de7b444b64abb859f16041fc9f
Carlos Lopez <cglbd4@mail.umkc.edu>
2022-11-17 14:59:33 -0600

    adding images and making a few changes to dummy data fundraiser info

--- DummyInfo.py
+++ DummyInfo.py
@@ -20,20 +20,20 @@ def main():
     userInfo = [("fqrqwqy", "0verth3rqinb0w", "kansasgirl@gmail.com", "Dorothy Gale", "202-918-2132", 40293, "4042 Nowear Lane", "Fields", "KS", "United States", 3589481030276534, "2024-12-" + str(monthLengths(12, 2024)), 112549572, 72254548),
                 ("clownschool", "DrunkenClown", "clownin@yahoo.com", "Honk McWaxxer", "320-577-7956", 23464, "1111 State Street", "Blowhorn", "AK", "United States", 4715949713735849, "2025-04-" + str(monthLengths(4, 2025)), 670826054, 71523538),
                 ("HomewardBound", "RADIO", "DenverNotColorodo@gmail.com", "John Denver", "582-482-0588", 48104, "1971 Country Road", "Mountain Mama", "WV", "United States", 4572307398817873, "2023-06-" + str(monthLengths(6, 2023)), 868484062, 70649423),
-                ("Lutece1879", "FreeBird", "2ofakind@gmail.com", "####### Lutece", "582-203-0838", 10902, "552 Upward Lane", "Columbia", "VA", "United States", 378594259397756, "2026-04-" + str(monthLengths(4, 2026)), 534369903, 93525636),
+                ("Lutece1879", "FreeBird", "2ofakind@gmail.com", "Lutece", "582-203-0838", 10902, "552 Upward Lane", "Columbia", "VA", "United States", 378594259397756, "2026-04-" + str(monthLengths(4, 2026)), 534369903, 93525636),
                 ("Kira1966", "BitesTheDust", "kirayoshikage@aol.com", "Kira Yoshikage", "318-659-6412", 77200, "4156 Villa Road", "Morioh", None, "Japan", 341179894763895, "2025-02-" + str(monthLengths(2, 2025)), 556649355, 31090784),
-                ("NotAScammer", "businessmogel", "workingfromhome@gmail.com", "Real Person", "309-611-6048", 82985, "12345 Main Street", "New York City", "NY", "United States", 5198066549559914, "2024-09-" + str(monthLengths(9, 2024)), 358769721, 51162661),
+                ("NotAScammer", "businessmogel", "workingfromhome@gmail.com", "Mac Jones", "309-611-6048", 82985, "12345 Main Street", "New York City", "NY", "United States", 5198066549559914, "2024-09-" + str(monthLengths(9, 2024)), 358769721, 51162661),
                 ("BigSturdy", "Doctor's Office", "personalcomputer@outlook.com", "William Doors", "203-952-2952", 52185, "645 Silicon Alley", "San Jose", "CA", "United States", 4848900679160589, "2024-08-" + str(monthLengths(8, 2024)), 77522155, 56915407),
                 ("Murphy's Law", "OCPStandardPassword", "Tinman@gmail.com", "Alex Murphy", "301-387-4911", 63453, "50399 Justice Road", "Detroit", "MI", "United States", 4599020519227307, "2026-05-" + str(monthLengths(5, 2026)), 270393550, 46045801),
                 ("FirstConsul", "AverageHeightForTheTime", "nbonaparte@yahoo.com", "Napoleon Bonaparte", "582-333-0707", 85947, "4154 Fraternity Street", "Paris", None, "France", 6011352349624030, "2026-01-" + str(monthLengths(1, 2026)), 940650316, 31487884),
                 ("LastConsul", "AcrosstheRhine", "venividivici@outlook.com", "Julius Caesar", "213-708-6937", 61422, "415 SPQR Street", "Rome", None, "Italy", 5456454184555214, "2026-09-" + str(monthLengths(9, 2026)), 663143155, 36846023)]
 
     #Fundraiser Info: Owner Email, Fund ID, Title, Description, Goal, Creation Date, Timeframe
-    FundInfo = [("kansasgirl@gmail.com", "Journey Home", "Dorothy wants to get off the Wizard's Wild Ride.", 1500, "2022-04-16 10-31-22", "2022-10-1 23-59-59"),
-                ("clownin@yahoo.com", "Accident Academy", "There's one born every minute. It takes a lot longer to refine them, though.", 2000, "2022-06-10 09-42-02", "2023-07-11 10-28-40"),
+    FundInfo = [("kansasgirl@gmail.com", "Journey Home", "Dorothy wants to get off the Wizard's Wild Ride and get back home to Kansas.", 1500, "2022-04-16 10-31-22", "2022-10-1 23-59-59"),
+                ("clownin@yahoo.com", "Misfit Academy", "Help fund this academy for impoverished children.", 2000, "2022-06-10 09-42-02", "2023-07-11 10-28-40"),
                 ("workingfromhome@gmail.com", "Investment Opportunity Going Fast!!", "Be the hottest invester you know! With me! Trusted by Warren Buffett himself! 10% monthly return guaranteed!! !", 100000, "2022-10-31 10-00-00", "2025-12-31 23-59-59"),
-                ("nbonaparte@yahoo.com", "Fonds d'invasion", "Pretendre que c'est suffisant pour envahir la Russie.", 50000, "2022-07-04 13-14-36", "2023-01-01 00-00-00"),
-                ("venividivici@outlook.com", "Incursio Pecunia", "Simulate hoc satis est ad incursionem Galliae.", 30000, "2022-05-05 15-55-13", "2023-08-05 00-00-00")]
+                ("nbonaparte@yahoo.com", "Joplin Disaster", "Raising funds for disaster relief in Joplin MO", 50000, "2022-07-04 13-14-36", "2023-01-01 00-00-00"),
+                ("venividivici@outlook.com", "I DO!!", "Help John and Kate realize their dream wedding!", 30000, "2022-05-05 15-55-13", "2023-08-05 00-00-00")]
 
     #Transaction Info: Transaction ID, Fundraiser ID, Donor Email, Donation Amount, Transaction Date
     TranInfo = [(1001, "DenverNotColorodo@gmail.com", 150, "2022-06-23 14-50-33"),
